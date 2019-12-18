d017a83d70cf5f9cb94000e5eb9b8772a94575d9
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2019-12-18 08:36:20 +0100

    Support Templates in DotNet Core

--- dotnet-core/RequestUriConfiguration.cs
+++ dotnet-core/RequestUriConfiguration.cs
@@ -17,6 +17,12 @@ namespace Itsme
 
         [JsonProperty(PropertyName = "claims")]
         public List<Claim> Claims { get; set; }
+
+        [JsonProperty(PropertyName = "advanced_payment_template")]
+        public AdvancedPaymentTemplate AdvancedPaymentTemplate { get; set; }
+
+        [JsonProperty(PropertyName = "free_text_template")]
+        public FreeTextTemplate FreeTextTemplate { get; set; }
     }
 
     [JsonConverter(typeof(StringEnumConverter))]
@@ -33,5 +39,24 @@ namespace Itsme
         ACRBasic,
         ACRAdvanced,
         ACRSecured
+    }
+
+    public class AdvancedPaymentTemplate
+    {
+        [JsonProperty(PropertyName = "amount")]
+        public string Amount { get; set; }
+
+
+        [JsonProperty(PropertyName = "currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty(PropertyName = "iban")]
+        public string IBAN { get; set; }
+    }
+
+    public class FreeTextTemplate
+    {
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
     }
 }
