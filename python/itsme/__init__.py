ba2564ff8610d3f83ff5fb6de542db484860445b
Giorgi Rostomashvili <giorgi.rostomashvili@inthepocket.com>
2020-02-04 16:28:29 +0100

    updated user model for python binding

--- python/itsme/__init__.py
+++ python/itsme/__init__.py
@@ -1,1 +1,3 @@
-from .itsme import Client, User, ItsmeSettings, UrlConfiguration, RequestURIConfiguration, RedirectData, PRODUCTION, SANDBOX
+from .itsme import (Client, User, ItsmeSettings, UrlConfiguration, RequestURIConfiguration, RedirectData, PRODUCTION,
+                    SANDBOX, ACRBasic, ACRAdvanced, ACRSecured, ClaimEid, ClaimCityOfBirth, ClaimNationality,
+                    ClaimDevice, ClaimPhoto)
