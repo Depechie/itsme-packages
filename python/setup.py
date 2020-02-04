38e6da35bfe278df0b81a6d43cba26707c0d90fa
Giorgi Rostomashvili <giorgi.rostomashvili@inthepocket.com>
2020-02-04 15:21:07 +0100

    fixed bash script for dependencies

--- python/setup.py
+++ python/setup.py
@@ -7,10 +7,15 @@ lib_version = '0.5.0.1579093712'
 version = '0.0.7'
 location = './itsme'
 
+fetch_deps_exit_code = None
+
 if operating_system == 'nt':
-    call(f'../scripts/dependencies.ps1 -libVersion {lib_version} -location {location}', shell=True)
+    fetch_deps_exit_code = call(f'../scripts/dependencies.ps1 -libVersion {lib_version} -location {location}', shell=True)
 else:
-    call(f'../scripts/dependencies.sh {lib_version} {location}', shell=True)
+    fetch_deps_exit_code = call(f'../scripts/dependencies.sh {lib_version} {location}', shell=True)
+
+if fetch_deps_exit_code != 0:
+    raise SystemExit("Failed to fetch dependencies")
 
 setup(
     name='itsme',
