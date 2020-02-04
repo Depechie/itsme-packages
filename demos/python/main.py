91bf158c75a115331835b1dd58c20d6ab9e5f03f
Giorgi Rostomashvili <giorgi.rostomashvili@inthepocket.com>
2020-02-04 16:56:50 +0100

    updated demo

--- demos/python/main.py
+++ demos/python/main.py
@@ -4,20 +4,25 @@ import itsme
 
 app = Flask(__name__)
 
+
 def _get_itsme_client():
     private_jwk_set = ''
     with open('../keys/jwks_private.json', 'r') as jwks_file:
         private_jwk_set = jwks_file.read()
-    client_id = 'my_client_id'
-    redirect_url = 'https://example.com/production/redirect'
+    client_id = 'client_id'
+    redirect_url = 'https://example.com/redirect'
     environment = itsme.PRODUCTION
-    settings = itsme.ItsmeSettings(client_id, redirect_url, private_jwk_set, environment)
+    settings = itsme.ItsmeSettings(
+        client_id, redirect_url, private_jwk_set, environment)
     return itsme.Client(settings)
 
-@app.route("/production/login")
+
+@app.route("/login")
 def login():
-    request_uri = 'https://example.com:443/production/request_uri'
-    config = itsme.UrlConfiguration(['profile', 'email', 'address', 'phone'], 'my_service_code', request_uri)
+    request_uri = 'https://example.com:443/request'
+    redirect_uri = 'https://example.com/redirect'
+    config = itsme.UrlConfiguration(
+        ['profile', 'email', 'address', 'phone'], 'service_code', redirect_uri, request_uri)
     itsme_auth_url = _get_itsme_client().get_authentication_url(config)
     body = {
         'url': itsme_auth_url
@@ -26,8 +31,8 @@ def login():
     return resp
 
 
-@app.route("/production/jwks.json")
-def hello():
+@app.route("/jwks")
+def jwks():
     jwks = ""
     with open('../keys/jwks_public.json') as f:
         jwks = f.read()
@@ -36,24 +41,32 @@ def hello():
     return resp
 
 
-@app.route("/production/redirect")
-def redirect():
+@app.route("/redirect")
+def redirect_url():
     code = request.args.get('code')
-    user = _get_itsme_client().get_user_details(code)
+    redirect_uri = 'https://example.com/redirect'
+    redirect_data = itsme.RedirectData(code, redirect_uri)
+    user = _get_itsme_client().get_user_details(redirect_data)
     resp = Response(dumps(user.__dict__, default=lambda o: o.__dict__))
     resp.headers['Content-Type'] = 'application/json'
     return resp
 
 
-@app.route("/production/request")
+@app.route("/request")
 def request_uri():
-    url_config = itsme.UrlConfiguration(['profile', 'email', 'address', 'phone'], 'my_service_code', '')
-    config = itsme.RequestURIConfiguration(url_config, 'tag:sixdots.be,2016-06:acr_advanced', 'nonce', 'state', ['tag:sixdots.be,2016-06:claim_city_of_birth', 'tag:sixdots.be,2016-06:claim_nationality', 'tag:sixdots.be,2017-05:claim_device', 'tag:sixdots.be,2016-06:claim_eid', 'tag:sixdots.be,2017-05:claim_photo'])
+    url_config = itsme.UrlConfiguration(
+        ['profile', 'email', 'address', 'phone'], 'service_code', 'https://example.com/redirect', '')
+    config = itsme.RequestURIConfiguration(url_config, itsme.ACRAdvanced, 'nonce', 'state', [
+                                           itsme.ClaimCityOfBirth, itsme.ClaimDevice, itsme.ClaimEid, itsme.ClaimNationality])
     data = _get_itsme_client().create_request_uri_payload(config)
     resp = Response(data)
     resp.headers['Content-Type'] = 'text/plain; charset=utf-8'
-
     return resp
+
+
+@app.route("/")
+def hello():
+    return "Hello!"
 
 
 if __name__ == '__main__':
