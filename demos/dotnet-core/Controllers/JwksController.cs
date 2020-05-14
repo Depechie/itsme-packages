74c2be2920410bbb19ccd4cb978a014fe22c67c0
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2020-05-14 14:24:20 +0200

    Expose public keys iso private

--- demos/dotnet-core/Controllers/JwksController.cs
+++ demos/dotnet-core/Controllers/JwksController.cs
@@ -9,7 +9,7 @@ namespace dotnet_core_api.Controllers
         [HttpGet()]
         public ActionResult<string> Get()
         {
-            var jwks = System.IO.File.ReadAllText("../keys/jwks_private.json");
+            var jwks = System.IO.File.ReadAllText("../keys/jwks_public.json");
             return Content(jwks, "application/json");
         }
     }
